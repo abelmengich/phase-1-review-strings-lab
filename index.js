131fdcc8ad3eec55cc5aea4963e66a5f9eccb5fc
abel mengich <kipyegoabel2308@gmail.com>
2024-03-19 16:25:42 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ${Grace Hopper}!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ${currentUser.toUpperCase(GRACE HOPPER)}!';
+const shortGreeting = 'Welcome, ${currentUser.charAt(G)}!';
+
+module.exports =  Welcome, excitedWellcomeMessage, shortGreeting ;
